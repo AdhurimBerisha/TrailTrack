dbb6dd8a0314947074bae381fd92221bc9438f42
AdhurimBerisha <adhurimberisha07@gmail.com>
2025-10-14 13:06:47 +0200

    Helper Styling Components

--- src/screens/TrackListScreen.js
+++ src/screens/TrackListScreen.js
@@ -4,7 +4,7 @@ import { Text, View, StyleSheet, Button } from "react-native";
 const TrackListScreen = ({ navigation }) => {
   return (
     <>
-      <Text style={{ fontSize: 48 }}>TrackListScreen</Text>;
+      <Text style={{ fontSize: 48 }}>TrackListScreen</Text>
       <Button
         title="Go to Track Detail"
         onPress={() => navigation.navigate("TrackDetail")}
