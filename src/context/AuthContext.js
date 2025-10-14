e3191304ed6cbe24a370be48958ec6b2ff5bc3a3
AdhurimBerisha <adhurimberisha07@gmail.com>
2025-10-14 14:19:27 +0200

    Handling Error Requests

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -3,6 +3,8 @@ import trackerAPI from "../api/tracker";
 
 const authReducer = (state, action) => {
   switch (action.type) {
+    case "add_error":
+      return { ...state, errorMessage: action.payload };
     default:
       return state;
   }
@@ -14,7 +16,10 @@ const signup = (dispatch) => {
       const response = await trackerAPI.post("/signup", { email, password });
       console.log(response.data);
     } catch (err) {
-      console.log(err.message);
+      dispatch({
+        type: "add_error",
+        payload: "Something went wrong with sign up",
+      });
     }
   };
 };
@@ -35,5 +40,5 @@ const signout = (dispatch) => {
 export const { Provider, Context } = createDataContext(
   authReducer,
   { signin, signout, signup },
-  { isSignedIn: false }
+  { isSignedIn: false, errorMessage: "" }
 );
