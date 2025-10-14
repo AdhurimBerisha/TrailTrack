a707c248c5c5a880861985222f3ee93877b5eb54
AdhurimBerisha <adhurimberisha07@gmail.com>
2025-10-14 22:54:15 +0200

    Drawing a Series of Points

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,18 +1,35 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
-import MapView from "react-native-maps";
+import { Text, StyleSheet } from "react-native";
+import MapView, { Polyline } from "react-native-maps";
 
 const Map = () => {
+  let points = [];
+  for (let i = 0; i < 20; i++) {
+    if (i % 2 === 0) {
+      points.push({
+        latitude: 37.33233 + i * 0.001,
+        longitude: -122.03121 + i * 0.001,
+      });
+    } else {
+      points.push({
+        latitude: 37.33233 - i * 0.002,
+        longitude: -122.03121 + i * 0.001,
+      });
+    }
+  }
+
   return (
     <MapView
-      initialRegion={{
-        latitude: 42.6629,
-        longitude: 21.1655,
-        latitudeDelta: 0.05,
-        longitudeDelta: 0.05,
-      }}
       style={styles.map}
-    />
+      initialRegion={{
+        latitude: 37.33233,
+        longitude: -122.03121,
+        latitudeDelta: 0.01,
+        longitudeDelta: 0.01,
+      }}
+    >
+      <Polyline coordinates={points} />
+    </MapView>
   );
 };
 
