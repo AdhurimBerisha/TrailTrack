64836bc2661cfb05236e95f7a9d29ab1d8219ba3
AdhurimBerisha <adhurimberisha07@gmail.com>
2025-10-17 12:54:05 +0200

    Automatic Authentication

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,6 +1,22 @@
 import axios from "axios";
 import { API_URL } from "@env";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-export default axios.create({
+const instance = axios.create({
   baseURL: API_URL,
 });
+
+instance.interceptors.request.use(
+  async (config) => {
+    const token = await AsyncStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (err) => {
+    return Promise.reject(err);
+  }
+);
+
+export default instance;
