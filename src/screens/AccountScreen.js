54aff4624ad7067ed8aedc59b5fb13eb7f804a63
AdhurimBerisha <adhurimberisha07@gmail.com>
2025-10-17 13:16:07 +0200

    Fixing Odds and Ends

--- src/screens/AccountScreen.js
+++ src/screens/AccountScreen.js
@@ -1,9 +1,10 @@
 import React, { useContext } from "react";
-import { Text, View, StyleSheet } from "react-native";
+import { View, StyleSheet, Text } from "react-native";
 import { Button } from "react-native-elements";
 import { SafeAreaView } from "react-navigation";
 import Spacer from "../components/Spacer";
 import { Context as AuthContext } from "../context/AuthContext";
+import { FontAwesome } from "@expo/vector-icons";
 
 const AccountScreen = () => {
   const { signout } = useContext(AuthContext);
@@ -16,6 +17,11 @@ const AccountScreen = () => {
       </Spacer>
     </SafeAreaView>
   );
+};
+
+AccountScreen.navigationOptions = {
+  title: "Account",
+  tabBarIcon: <FontAwesome name="gear" size={20} />,
 };
 
 const styles = StyleSheet.create({});
